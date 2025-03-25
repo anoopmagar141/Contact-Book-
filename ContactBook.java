c8fe40f23103baf697a1c95ce1d09baf6364c441
[Anup  Magar] <am6956275@gmail.com>
2025-03-25 22:42:01 +0545

    Add Contact class with attributes and methods for name, phone, and email

--- ContactBook.java
+++ ContactBook.java
@@ -3,3 +3,21 @@ public class ContactBook {
         System.out.println("Welcome to Contact Book!");
     }
 }
+class Contact {
+    private String name, phone, email;
+
+    public Contact(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + " | Phone: " + phone + " | Email: " + email;
+    }
+}
